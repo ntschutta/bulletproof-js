fbf388bbf3327bd5394df14a8c1ba6dd6eb8e032
Nate Schutta <ntschutta@gmail.com>
2017-03-15 20:18:38 -0500

    added line to fix error for latest version of jshint

--- examples/angularjs/Gruntfile.js
+++ examples/angularjs/Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) { 
     jshint: {
       files: ['js/**/*.js', 'test/unit/*.js'],
       options: {
+        reporterOutput: "",
         //curly: true,
         //eqeqeq: true,
         //noempty: true,
