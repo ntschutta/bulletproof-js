c41a3d9ea9a5480e46fad34377df093390ebdfd6
Nate Schutta <ntschutta@gmail.com>
2017-03-15 20:21:36 -0500

    added option to resolve issue after upgrading to latest jshint

--- examples/angularjs/Gruntfile.js
+++ examples/angularjs/Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) { 
     jshint: {
       files: ['js/**/*.js', 'test/unit/*.js'],
       options: {
+        reporterOutput: "",
         '-W117': true,
         jshintrc: true,
         //jshintrc: '../../.jshintrc'
